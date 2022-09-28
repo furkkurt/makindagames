76dce47b066cd774cc5d95f2ca1d361e5d02c54f
root <afurkankurt@outlook.com>
2022-09-28 12:54:40 +0300

    Update

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    screens: {
+      "sm": "768px",
+      "md": "976px",
+      "lg": "1440px",
+      "xl": "1920px"
+    },
+    extend: {
+      
+    },
+  },
+  plugins: [],
+}
